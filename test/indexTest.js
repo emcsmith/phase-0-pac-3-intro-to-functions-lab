7eb624768c694ceba608a5ed92ac82fb9fda8367
Emily Smith <emcsmithh@gmail.com>
2023-03-13 00:24:40 -0400

    Add package-lock.json

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,12 +6,18 @@ describe('shout(string)', function() {
     expect(shout('hello')).toEqual('HELLO');
   })
 })
+function shout(string) {
+  return string.toUpperCase();
+}
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
   })
 })
+function whisper(string) {
+  return string.toLowerCase();
+}
 
 describe('logShout(string)', function() {
   it('takes a string argument and logs it in all caps using console.log()', function() {
@@ -47,8 +53,25 @@ describe('sayHiToHeadphonedRoommate(string)', function() {
   })
 
   it('returns "I would love to!" if `string` is "Let\'s have dinner together!"`', function () {
-    expect(sayHiToHeadphonedRoommate("Let's have dinner together!")).toEqual(
+    expect(sayHiToHeadphonedRoommate("Let\'s have dinner together!")).toEqual(
       "I would love to!"
     );
   });
 })
+
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if(string === "hello")
+     return"I can\'t hear you!";
+  else if (string === "HELLO")
+    return"YES INDEED!";
+  else if (string === "Let\'s have dinner together!")
+    return"I would love to!"
+}
